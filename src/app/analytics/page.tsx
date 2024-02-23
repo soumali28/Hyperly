02dc5f6a2f457a33ff1909dc45bbbc9b3b732a85
soumali28 <gorai.soumali@gmail.com>
2024-02-23 15:50:50 +0530

    analytics

--- src/app/analytics/page.tsx
+++ src/app/analytics/page.tsx
@@ -5,7 +5,168 @@ const Analytics = () => {
   return (
     <div>
       <Navbar />
-      Analytics
+      <div className="m-8">
+        <div className="overflow-x-auto">
+          <table className="table">
+            {/* head */}
+            <thead>
+              <tr>
+                <th>
+                  <label>
+                    <input type="checkbox" className="checkbox" />
+                  </label>
+                </th>
+                <th>Post</th>
+                <th>Date  </th>
+                <th>Views</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {/* row 1 */}
+              <tr>
+                <th>
+                  <label>
+                    <input type="checkbox" className="checkbox" />
+                  </label>
+                </th>
+                <td>
+                  <div className="flex items-center gap-3">
+                    <div className="avatar">
+                      <div className="mask mask-squircle w-12 h-12">
+                        <img
+                          src="/tailwind-css-component-profile-2@56w.png"
+                          alt="Avatar Tailwind CSS Component"
+                        />
+                      </div>
+                    </div>
+                    <div>
+                      <div className="font-bold">Hart Hagerty</div>
+                      <div className="text-sm opacity-50">United States</div>
+                    </div>
+                  </div>
+                </td>
+                <td>
+                  13th Jan 2024
+                  <br />
+                  <span className="badge badge-ghost badge-sm">
+                    Average
+                  </span>
+                </td>
+                <td>1.2k</td>
+                <th>
+                  <button className="btn btn-ghost btn-xs">details</button>
+                </th>
+              </tr>
+              {/* row 2 */}
+              <tr>
+                <th>
+                  <label>
+                    <input type="checkbox" className="checkbox" />
+                  </label>
+                </th>
+                <td>
+                  <div className="flex items-center gap-3">
+                    <div className="avatar">
+                      <div className="mask mask-squircle w-12 h-12">
+                        <img
+                          src="/tailwind-css-component-profile-3@56w.png"
+                          alt="Avatar Tailwind CSS Component"
+                        />
+                      </div>
+                    </div>
+                    <div>
+                      <div className="font-bold">Brice Swyre</div>
+                      <div className="text-sm opacity-50">China</div>
+                    </div>
+                  </div>
+                </td>
+                <td>
+                  5th Jan 2024
+                  <br />
+                  <span className="badge badge-ghost badge-sm">
+                    Bad
+                  </span>
+                </td>
+                <td>200</td>
+                <th>
+                  <button className="btn btn-ghost btn-xs">details</button>
+                </th>
+              </tr>
+              {/* row 3 */}
+              <tr>
+                <th>
+                  <label>
+                    <input type="checkbox" className="checkbox" />
+                  </label>
+                </th>
+                <td>
+                  <div className="flex items-center gap-3">
+                    <div className="avatar">
+                      <div className="mask mask-squircle w-12 h-12">
+                        <img
+                          src="/tailwind-css-component-profile-4@56w.png"
+                          alt="Avatar Tailwind CSS Component"
+                        />
+                      </div>
+                    </div>
+                    <div>
+                      <div className="font-bold">Marjy Ferencz</div>
+                      <div className="text-sm opacity-50">Russia</div>
+                    </div>
+                  </div>
+                </td>
+                <td>
+                  5min ago
+                  <br />
+                  <span className="badge badge-ghost badge-sm">
+                    Good
+                  </span>
+                </td>
+                <td>1.7k</td>
+                <th>
+                  <button className="btn btn-ghost btn-xs">details</button>
+                </th>
+              </tr>
+              {/* row 4 */}
+              <tr>
+                <th>
+                  <label>
+                    <input type="checkbox" className="checkbox" />
+                  </label>
+                </th>
+                <td>
+                  <div className="flex items-center gap-3">
+                    <div className="avatar">
+                      <div className="mask mask-squircle w-12 h-12">
+                        <img
+                          src="/tailwind-css-component-profile-5@56w.png"
+                          alt="Avatar Tailwind CSS Component"
+                        />
+                      </div>
+                    </div>
+                    <div>
+                      <div className="font-bold">Yancy Tear</div>
+                      <div className="text-sm opacity-50">Brazil</div>
+                    </div>
+                  </div>
+                </td>
+                <td>
+                  2nd Jan 2024
+                  <br />
+                  <span className="badge badge-ghost badge-sm">
+                    Excellent
+                  </span>
+                </td>
+                <td>2M</td>
+                <th>
+                  <button className="btn btn-ghost btn-xs">details</button>
+                </th>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 };
